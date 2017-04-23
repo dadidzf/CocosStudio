6f7dd1031fe693e342d5fdcd99fcc9d5a17681db
dengzhuangfeng <dengzhuangfeng@hotmail.com>
2017-04-23 14:25:22 +0800

    GameConfig change to json, to make it is esay for python to read for project configure

--- LuaTemplate/src/main.lua
+++ LuaTemplate/src/main.lua
@@ -2,16 +2,58 @@ cc.FileUtils:getInstance():setPopupNotify(false)
 
 require "config"
 require "cocos.init"
-require "GameConfig"
 
 math.randomseed(os.time())
 
-local function main()
-	if dd.isSelfAdsEnabled then
-		cc.load("sdk").MyAds.init()
+local function initBeforeGame()
+	local gameConf  = require("cjson").decode(cc.FileUtils:getInstance():getStringFromFile("src/GameConfig.json"))
+	for k, v in pairs(gameConf) do
+		dd[k] = v
 	end
+	--dd.serverConfig.serverHost = "http://127.0.0.1:5000/"
 
+	cc.load("sdk").MyAds.init()
+end
+
+local function main()
+	initBeforeGame()
     require("app.MyApp"):create():run()
+end
+
+cc.exports.__G__TRACKBACK__ = function (msg)
+    local msg = debug.traceback(msg, 3)
+    print(msg)
+
+    if not display.getRunningScene() then
+    	return
+    end
+    
+    local MAX_INT = 0x7fffffff
+  	local maskLayer = ccui.Layout:create()
+  		:setBackGroundColorType(LAYOUT_COLOR_SOLID)
+  		:setBackGroundColor(cc.BLACK)
+  		:setBackGroundColorOpacity(150)
+  		:setTouchEnabled(true)
+  		:setSwallowTouches(true)
+  		:setGlobalZOrder(MAX_INT)
+  		:setContentSize(display.size)
+  		:addTo(display.getRunningScene())
+
+	cc.Label:createWithSystemFont(msg, "", 32)
+		:setAnchorPoint(cc.p(0, 1))
+		:move(0, display.height)
+		:setWidth(display.width)
+		:addTo(maskLayer)
+
+	local closeBtn = ccui.Button:create("sdk_close_btn.png")
+		:setAnchorPoint(cc.p(1, 0))
+		:move(display.width, 0)
+		:addTo(maskLayer)
+		:setTouchEnabled(true)
+		:setGlobalZOrder(MAX_INT)
+		:onClick(function ()
+			maskLayer:removeFromParent()
+		end)
 end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
