e44ee29e5af9b3c7f26918b67569fa29392b7e74
dengzhuangfeng <dengzhuangfeng@hotmail.com>
2017-06-24 20:40:26 +0800

    trapballs project

--- LuaTemplate/frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/lua/GameHelperUtils.java
+++ LuaTemplate/frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/lua/GameHelperUtils.java
@@ -9,7 +9,7 @@ import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.games.GamesActivityResultCodes;
-import com.yongwuart.template.R;
+import com.yongwuart.trapballs.R;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
