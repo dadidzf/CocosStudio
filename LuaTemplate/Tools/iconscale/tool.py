d1a5e620630a1aad254fd6ad75860312073beaed
dengzhuangfeng <dengzhuangfeng@hotmail.com>
2017-11-06 22:16:14 +0800

    增加icon size 167x167

--- LuaTemplate/Tools/iconscale/tool.py
+++ LuaTemplate/Tools/iconscale/tool.py
@@ -6,7 +6,7 @@ from PIL import Image
 iosSizes = ['Icon-29', 'Icon-40', 'Icon-50', 'Icon-57', 
 			'Icon-58', 'Icon-72', 'Icon-76', 'Icon-80', 
 			'Icon-87', 'Icon-100','Icon-114', 'Icon-120', 
-			'Icon-144', 'Icon-152', 'Icon-180', 'Icon-1024']
+			'Icon-144', 'Icon-152', 'Icon-167', 'Icon-180', 'Icon-1024']
 androidSizes = [512]
 androidNames = ['ic_launcher']
 
