9cd4b5ae6093b250cf00eec4ed9acda48afc434a
dengzhuangfeng <dengzhuangfeng@gmail.com>
2018-03-22 20:39:33 +0800

    HeroChess 在线更新自动打包工具python初版

--- LuaTemplate/Tools/createManifest/autoManifest.py
+++ LuaTemplate/Tools/createManifest/autoManifest.py
@@ -1,61 +1,84 @@
 import os
 import hashlib
 import time
-#
-def getFileMd5(filename):
-    if not os.path.isfile(filename):
-        return
-    myhash = hashlib.md5()# create a md5 object
-    f = file(filename,'rb')
-    while True:
-        b = f.read(8096)# get file content.
-        if not b :
-            break
-        myhash.update(b)#encrypt the file
-    f.close()
-    return myhash.hexdigest()
 
-def walk(path, prefix):
-    global xml
-    fl = os.listdir(path) # get what we have in the dir.
+def getFileMd5(filename):
+    if os.path.isfile(filepath):
+        md5obj = hashlib.md5()
+        maxbuf = 8192
+        f = open(filepath,'rb')
+        while True:
+            buf = f.read(maxbuf)
+            if not buf:
+                break
+            md5obj.update(buf)
+        f.close()
+        hash = md5obj.hexdigest()
+        return str(hash).upper()
+    else:
+        return None
+
+def walk(path, assetsDict, prefix):
+    fl = os.listdir(path) 
     for f in fl:
         if os.path.isdir(os.path.join(path,f)): # if is a dir.
             if prefix == '':
-                walk(os.path.join(path,f), f)
+                walk(os.path.join(path, f), assetsDict, f)
             else:
-                walk(os.path.join(path,f), prefix + '/' + f)
+                walk(os.path.join(path,f), assetsDict, prefix + '/' + f)
         else:
-            md5 = getFileMd5(os.path.join(path,f))
-            xml += "\n\t\t\"%s\" : {\n\t\t\t\"md5\" : \"%s\"\n\t\t}, " % (prefix + '/' + f, md5) # output to the md5 value to a string in xml format.
+            md5 = getFileMd5(os.path.join(path, f))
+            if prefix == '':
+                assetsDict[f] = md5
+            else:
+                assetsDict[prefix + '/' + f] = md5
+
+def writeManifest(manifest, filePath):
+    strMan = json.dumps(manifest, encoding = 'utf8')
+    f = file(filePath, "w+")
+    f.write(strMan)
+    f.close()
+
+def process(srcdir, destdir, version, url):
+    manifest = {
+        'packageUrl': url,
+        'remoteManifestUrl': url + 'project.manifest',
+        'remoteVersionUrl': url + 'version.manifest',
+        'version': version,
+        'assets': {},
+        'searchPaths': []
+    };
+
+    writeManifest(manifest, os.path.join(destdir, 'version.manifest'))
+
+    if not os.path.exists(destdir):
+        os.mkdir(destdir)
+
+    walk(srcdir, manifest.assets, '')
+
+    writeManifest(manifest, os.path.join(destdir, 'project.manifest'))
 
 if __name__ == "__main__": 
-    timeStr = time.strftime("%Y%m%d%H%M%S",time.localtime(time.time()))
-    if not os.path.exists(os.getcwd() + '\\manifest'):
-        os.mkdir(os.getcwd() + '\\manifest')
-    #generate project.manifest
-    xml = '{\
-    \n\t"packageUrl" : "http://192.168.2.50/version/",\
-    \n\t"remoteVersionUrl" : "http://192.168.2.50/manifest/version.manifest",\
-    \n\t"remoteManifestUrl" : "http://192.168.2.50/manifest/project.manifest",\
-    \n\t"version" : "0.0.%s",\
-    \n\t"engineVersion" : "Cocos2d-x v3.10",\
-    \n\n\t"assets" : {' % timeStr
-    walk(os.getcwd() + '\\version', '')
-    xml = xml[:-2]
-    xml += '\n\t},\
-    \n\t"searchPaths" : [\
-    \n\t]\
-    \n}'
-    f = file("manifest\project.manifest", "w+")
-    f.write(xml)
-    print 'generate project.manifest finish.'
-    #generate version.manifest
-    xml = '{\
-    \n\t"packageUrl" : "http://192.168.2.50/api/version/",\
-    \n\t"remoteVersionUrl" : "http://192.168.2.50/api/manifest/version.manifest",\
-    \n\t"remoteManifestUrl" : "http://192.168.2.50/api/manifest/project.manifest",\
-    \n\t"version" : "0.0.%s",\
-    \n\t"engineVersion" : "Cocos2d-x v3.10"\n}' % timeStr
-    f = file("manifest\\version.manifest", "w+")
-    f.write(xml)
-    print 'generate version.manifest finish.'
+    curPath = os.path.split(os.path.realpath(__file__))[0]
+    srcdir = curPath
+    destdir = curPath
+    version = '1.0.0'
+    url = 'http://localhost:8080/'
+
+    index = 1
+    while index < len(sys.argv):
+        arg = sys.argv[index]
+        if arg == '-d' or arg == '--dest':
+            destdir = sys.argv[index + 1]
+        elif arg == '-v' or arg == '--version':
+            version = sys.argv[index + 1]
+        elif arg == '-u' or arg == '--url':
+            url = sys.argv[index + 1]
+        elif arg == '-v' or arg == '--version':
+            version = sys.argv[index + 1]
+        elif arg == '-s' or arg == '--src':
+            srcdir = sys.argv[index + 1]
+
+        index = index + 1
+
+    process(srcdir, destdir, version, url)
