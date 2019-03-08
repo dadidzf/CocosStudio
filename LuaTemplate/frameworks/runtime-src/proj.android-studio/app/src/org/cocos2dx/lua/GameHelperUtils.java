05633b6c5b40e3167c048be3e51c5cc06397018f
dengzhuangfeng <dengzhuangfeng@gmail.com>
2019-03-08 11:16:35 +0800

    备份

--- LuaTemplate/frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/lua/GameHelperUtils.java
+++ LuaTemplate/frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/lua/GameHelperUtils.java
@@ -9,7 +9,7 @@ import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.games.GamesActivityResultCodes;
-import com.yongwuart.trapballs.R;
+import cn.herofight.herochess.R;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
